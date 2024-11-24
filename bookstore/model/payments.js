940c8e91789e9cc5e04b6720e85dcbb9698c29eb
Yen1901 <117509770+Yen1901@users.noreply.github.com>
2024-11-24 12:50:42 +0700

    Delete bookstore directory

--- bookstore/model/payments.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const paymentSchema = new mongoose.Schema({
-    name: { type: String, required: true, unique: true },
-    orders: [{type: mongoose.Schema.Types.ObjectId, ref: 'Order'}]
-}, { timestamps: true });
-
-const Payment = mongoose.model('Payment', paymentSchema);
-module.exports = Payment;
